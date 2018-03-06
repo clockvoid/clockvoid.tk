6183db4b73f47b4798a4d1e386919d66e50b6fd9
clockvoid <hidenari.t1997nov@gmail.com>
2018-03-06 19:14:11 +0900

    fix: link size

--- index.php
+++ index.php
@@ -4,33 +4,45 @@ include './top.php';
 ?>
 <h1 style="font-weight: bold;">基礎のほーむぺーじ</h1>
 <p>
-こんにちは．基礎のホームページヘようこそ．ゆっくりしていってね．
-<br>
-現在工事中です．色々用意できていないページがありますが，
-ご容赦ください．
+    こんにちは．基礎のホームページヘようこそ．ゆっくりしていってね．
+    <br>
+    現在工事中です．色々用意できていないページがありますが，
+    ご容赦ください．
 </p>
 <div class="flex-container">
-<div id="element">
-<strong><a href="./about.php">About</a></strong>
-<br>
-<p>
-Aboutページです．プロフィールなど紹介しています．
-</p>
-</div>
-<div id="element">
-<strong><a href="./contact.php">Contact</a></strong>
-<br>
-<p>
-Contactページです．GitHubやTwitterなどはこちらから．
-</p>
-</div>
-<div id="element">
-<strong><a href="./reversi.php">Reversi</a></strong>
-<br>
-<p>
-デモで作成したReversiのコードです．一応ここにリンクを貼ります．
-</p>
-</div>
+    <div id="element">
+        <strong>
+            <a href="./about.php">
+                <p>About</p>
+            </a>
+        </strong>
+        <br>
+        <p>
+        Aboutページです．プロフィールなど紹介しています．
+        </p>
+    </div>
+    <div id="element">
+        <strong>
+            <a href="./contact.php">
+                <p>Contact</p>
+            </a>
+        </strong>
+        <br>
+        <p>
+        Contactページです．GitHubやTwitterなどはこちらから．
+        </p>
+    </div>
+    <div id="element">
+        <strong>
+            <a href="./reversi.php">
+                <p>Reversi</p>
+            </a>
+        </strong>
+        <br>
+        <p>
+        デモで作成したReversiのコードです．一応ここにリンクを貼ります．
+        </p>
+    </div>
 </div>
 <?php
 include './bottom.php';
