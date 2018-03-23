d82aa0f2a03eade16b9000f5b4dae663bd983036
clockvoid <hidenari.t1997nov@gmail.com>
2018-03-24 04:01:56 +0900

    fix: about page and some desing issues

--- about.php
+++ about.php
@@ -3,37 +3,45 @@ $seed=">B0ofMnbI=z5=VRt";
 include 'top.php';
 ?>
             <h1 style="font-weight: bold;">About me</h1>
-            <div class="row">
-              <div class="col-sm-3">
-
-                <img style="max-width: 100%; border-radius: 12px; margin-bottom: .8em;" src="https://pbs.twimg.com/profile_images/745222112273522688/-23LMFEG.jpg" alt="">
-
+                <center><img style="max-width: 30%; border-radius: 12px; margin-bottom: .8em;" src="https://pbs.twimg.com/profile_images/745222112273522688/-23LMFEG.jpg" alt=""></center>
                 <figcaption><center>↑ぼく</center></figcaption>
-              </div>
-              <div class="col-sm-9">
-                <h2><strong>名前</strong></h2>
-                <p style="text-indent: 5em;">clock_void</p>
-                <hr>
-                <h2><strong>職業</strong></h2>
-                <p style="text-indent: 5em;">大学生</p>
-                <hr>
-                <h2><strong>趣味</strong></h2>
-                <p style="text-indent: 5em;">プログラミング</p>
-                <hr>
-                <h2><strong>使用環境</strong></h2>
-                <p style="text-indent: 5em;">OS</p>
-                <p style="text-indent: 8em;">Arch Linux</p>
-                <p style="text-indent: 8em;">Windows 10</p>
-
-                <p style="text-indent: 5em;">X Window Manager</p>
-                <p style="text-indent: 8em;">XMonad</p>
-
-                <p style="text-indent: 5em;">テキストエディタ</p>
-                <p style="text-indent: 8em;">Vim</p>
-                <p style="text-indent: 8em;">Neovim</p>
-                <hr>
-              </div>
-            </div>
+              <br>
+<table style="border-bottom-color: #dddddd; border-bottom-width: 1px; border-bottom-style: solid;">
+    <tbody>
+        <tr>
+            <th>名前</th>
+            <td>基礎</td>
+        </tr>
+        <tr>
+            <th>職業</th>
+            <td>大学生</td>
+        </tr>
+        <tr>
+            <th>趣味</th>
+            <td>プログラミング</td>
+        </tr>
+        <tr>
+            <th>使用環境</th>
+            <td>
+                <p>OS</p>
+                <ul>
+                    <li>Arch Linux</li>
+                    <li>Windows 10</li>
+                </ul>
+                <p>X Window Manager</p>
+                <ul>
+                    <li>XMonad</li>
+                    <li>XMobar</li>
+                </ul>
+                <p>Editor</p>
+                <ul>
+                    <li>Vim</li>
+                    <li>Neovim</li>
+                    <li>Intellij</li>
+                </ul>
+            </td>
+    </tbody>
+</table>
 <?php
 include 'bottom.php'
 ?>
