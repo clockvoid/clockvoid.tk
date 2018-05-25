b96165e8ab2c636922c05aa42003fa521b4e116b
clockvoid <hidenari.t1997nov@gmail.com>
2018-05-26 02:56:56 +0900

    fix: shibuyaapk25

--- blog.php
+++ blog.php
@@ -23,6 +23,18 @@ if (isset($_GET['content'])) {
 <div class="flex-container">
     <div id="element">
         <strong>
+            <a href="./blog.php?content=shibuyaapk25">
+                <p>Shibuya.apk #25に行ってきました．</p>
+            </a>
+        </strong>
+        <br>
+        <p class="date">2018-05-25</p>
+        <p>
+            Shibuya.apk #25に行ってきたのでまとめてみます．
+        </p>
+    </div>
+    <div id="element">
+        <strong>
             <a href="./blog.php?content=xorg_virtualbox">
                 <p>XorgとVirtualBox Guest Additionのバージョン問題</p>
             </a>
